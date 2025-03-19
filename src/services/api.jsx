ace6b2c0a39afec641c2fd1ab73be6b80ae11d77
Satheesh-Baabu <satheeshbaabum@gmail.com>
2025-03-19 11:23:53 +0530

    Api changed in .env

--- src/services/api.jsx
+++ src/services/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://projectrestaurantbackend.onrender.com";
 
 export const register = async (userData) => {
   return axios.post(`${API_BASE_URL}/api/auth/register`, userData);
